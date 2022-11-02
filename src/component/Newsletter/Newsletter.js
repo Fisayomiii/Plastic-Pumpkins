b89de9414786f1a7af506ba7cd61b587aa96c11a
oluwafisayomi <wrld26993@gmail.com>
2022-11-02 11:49:00 +0100

    added sweet alert to the newsletter

--- src/component/Newsletter/Newsletter.js
+++ src/component/Newsletter/Newsletter.js
@@ -1,6 +1,32 @@
-import React from 'react';
-
+import React, { useRef} from 'react'
+import Swal from 'sweetalert2';
 function Newsletter() {
+    const emailRef = useRef();
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        const formValue = emailRef.current.value;
+
+        if (
+            formValue.trim().length > 0 && formValue.includes("@") && formValue.includes(".") && formValue.includes("com")
+        )
+        {
+            Swal.fire({
+                title: "Successful!",
+                text: "Your email has been added to our list.",
+                icon: "success",
+            });
+            event.target.reset();
+        } else {
+            Swal.fire({
+                title: "OOPS!",
+                text: "Please input a valid email address.",
+                icon: "error",
+            });
+        }
+    };
+    
     return (
         <>
             {/* OUR NEWSLETTER */}
@@ -11,9 +37,9 @@ function Newsletter() {
                         Promotion new products and sales. Directly to your inbox
                     </p>
 
-                    <form action="" className="newsletter__form">
-                        <input type="email" placeholder="Enter your email" className="newsletter__input" />
-                        <button className="button">Subscribe</button>
+                    <form className="newsletter__form" onSubmit={onSubmitHandler}>
+                        <input type="email" placeholder="Enter your email" className="newsletter__input" ref={emailRef}/>
+                        <button type='submit'  className="button">Subscribe</button>
                     </form><br />
                 </div>
             </section>
